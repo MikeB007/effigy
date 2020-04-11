1b62fee084209308385b3bd88a554c7edf1973fe
kogutc <kogutc@hotmail.com>
2020-04-10 20:59:17 -0400

    user Fri 04/10/2020 20:59:17.87   comments

--- gpush.bat
+++ gpush.bat
@@ -1,5 +1,18 @@
 git add *
 git status
 set dt = date /T
-git commit -m "%USERNAME% %date% %time% %1"
+
+@echo off
+set RESTVAR=
+shift
+:loop1
+if "%1"=="" goto after_loop
+set RESTVAR=%RESTVAR% %1
+shift
+goto loop1
+
+:after_loop
+echo %RESTVAR%
+
+git commit -m "%USERNAME% %date% %time% %1 %RESTVAR%"
 git push 
