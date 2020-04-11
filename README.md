3f99dc24af02beda8eac1316eca4fb3e79ca1d31
kogutc <kogutc@hotmail.com>
2020-04-11 17:02:14 -0400

    Dev (#2)
    * db script
    
    * db script
    
    * stash
    
    * Stash
    
    * Fri 04/10/2020
    
    * Fri 04/10/2020
    
    * CKFri 04/10/2020
    
    * CKFri 04/10/2020_20:17:16.91
    
    * CK user Fri 04/10/2020 20:20:26.33
    
    * user Fri 04/10/2020 20:20:48.88
    
    * user Fri 04/10/2020 20:54:14.74 'adding
    
    * user Fri 04/10/2020 20:59:17.87   comments
    
    * user Fri 04/10/2020 21:00:18.51    2 3 4
    
    * user Fri 04/10/2020 21:03:58.55   $1 2 3 4
    
    * user Fri 04/10/2020 21:04:19.43   $1 2 3 4
    
    * user Fri 04/10/2020 21:04:51.65 1 2 3 4
    
    * user Fri 04/10/2020 21:05:44.48 adding comments
    
    * user Fri 04/10/2020 21:06:09.01
    
    * user Fri 04/10/2020 21:10:08.55 YEAH YEAH
    
    * user Sat 04/11/2020 14:18:43.53 database schema creation and database model
    
    * user Sat 04/11/2020 16:53:54.53
    
    * user Sat 04/11/2020 16:55:59.28 changed readme
    
    Co-authored-by: EricKogut <eric.jkogut@gmail.com>

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # effigy
 This project will be a MEAN stack application meant for users to view and share their pictures.
 
-
 History:
 CK - added github script
--- gpush.bat
+++ gpush.bat
@@ -1,5 +1,20 @@
 git add *
 git status
 set dt = date /T
-git commit -m "%USERNAME% %date% %time%"
+
+
+@echo off
+REM ADD COMMENTS '20 ck YEAH
+set RESTVAR=%1
+shift
+:loop1
+if "%1"=="" goto after_loop
+set RESTVAR=%RESTVAR% %1
+shift
+goto loop1
+
+:after_loop
+echo %RESTVAR%
+
+git commit -m "%USERNAME% %date% %time% %RESTVAR%"
 git push 
