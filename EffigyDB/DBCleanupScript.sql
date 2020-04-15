0faa1a4bc5a4d924a8e840e84d56c296a8fa0278
kogutc <kogutc@hotmail.com>
2020-04-15 01:52:13 -0400

    user Wed 04/15/2020  1:52:13.36 added new model

--- EffigyDB/DBCleanupScript.sql
+++ EffigyDB/DBCleanupScript.sql
@@ -1,5 +1,6 @@
--- delete from loc_folder;
--- delete from loc_root;
+delete from loc_folder;
+delete from loc_root;
 DELETE FROM media_type;
 delete from supported_ext;
 delete from skip_root;
+delete media;
